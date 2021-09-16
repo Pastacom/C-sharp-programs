ed9eb534319e39a6e44342612bd970dc29be2a9f
Pastacom <mailkiborg@mail.ru>
2021-09-16 20:47:42 +0300

    .

--- Module_1/Lesson_2/HW/Task06/Task06.cs
+++ Module_1/Lesson_2/HW/Task06/Task06.cs
@@ -2,16 +2,36 @@
 using System.Globalization;
 class Program
 {
-    static void Calculation(decimal budget, decimal percentage)
+    static void Calculation(decimal budget, decimal percentage, uint currency)
     {
-        string res = (budget * (percentage / 100)).ToString("C", new CultureInfo("en-US"));
-        Console.WriteLine("На игры будет выделено: " + res);
+        string res;
+        switch (currency)
+        { 
+            case 1:
+                res = (budget * (percentage / 100)).ToString("C", new CultureInfo("en-US"));
+                Console.WriteLine("На игры будет выделено: " + res);
+                break;
+            case 2:
+                res = (budget * (percentage / 100)).ToString("C", new CultureInfo("fr-FR"));
+                Console.WriteLine("На игры будет выделено: " + res);
+                break;
+            case 3:
+                res = (budget * (percentage / 100)).ToString("C", new CultureInfo("ru-RU"));
+                Console.WriteLine("На игры будет выделено: " + res);
+                break;
+        }
+        
     }
     static void Main()
     {
-        string st; decimal budget; decimal percentage;
+        string st; decimal budget; decimal percentage; uint currency;
         do
         {
+            do
+            {
+                Console.Write("Валюты:\n1. Доллары\n2. Евро\n3. Рубли\nВведите цифру, под которым находится нужная вам валюта: ");
+                st = Console.ReadLine();
+            } while (!(uint.TryParse(st, out currency) && (0 < currency) && (currency < 4)));
             do
             {
                 Console.Write("Введите свой бюджет: ");
@@ -22,7 +42,7 @@ class Program
                 Console.Write("Введите процент выделяемый на игры: ");
                 st = Console.ReadLine();
             } while (!(decimal.TryParse(st, out percentage) && (percentage > 0)));
-            Calculation(budget, percentage);
+            Calculation(budget, percentage, currency);
             Console.WriteLine("Для выхода нажмите Esc. Для продолжения нажмите любую кнопку.");
         } while (Console.ReadKey().Key != ConsoleKey.Escape);
     }
