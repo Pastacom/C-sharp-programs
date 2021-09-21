92a4c6f5977ed29ee6d4083d0216b72b236e2ed6
Pastacom <mailkiborg@mail.ru>
2021-09-21 19:22:18 +0300

    .

---
+++ Module_1/Lesson_5/CW/Task01/Task01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+
+    static void Sums(uint number, out uint sumEven, out uint sumOdd)
+    {
+        sumEven = 0; sumOdd = 0;
+        while (number > 0)
+        {
+            sumEven += number % 10;
+            number /= 10;
+            sumOdd += number % 10;
+            number /= 10;
+        }
+    }
+    static void Main()
+    {
+        string st; uint number;
+        do
+        {
+            do
+            {
+                Console.Write("Введите число: ");
+                st = Console.ReadLine();
+            } while (!(uint.TryParse(st, out number)));
+            Sums(number, out uint sumEven, out uint sumOdd);
+            Console.WriteLine($"Сумма четных = {sumEven}, сумма нечетных = {sumOdd}");
+            Console.WriteLine("Для выхода нажмите Esc. Для продолжения нажмите любую кнопку.");
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+    }
+}
---
+++ Module_1/Lesson_5/CW/Task02/Task02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+
+    static void Sums(uint k)
+    {
+        for (int n = 1; n <= k; n++)
+        {
+            double s = 0; 
+            for (int i = 1; i <= n; i++)
+            {
+                s += (i + 0.3) / (3 * i * i + 5);
+            }
+            Console.WriteLine($"S(n = {n}) = {s}");
+
+        }
+    }
+    static void Main()
+    {
+        string st; uint k;
+        do
+        {
+            do
+            {
+                Console.Write("Введите число: ");
+                st = Console.ReadLine();
+            } while (!(uint.TryParse(st, out k)));
+            Sums(k);
+            Console.WriteLine("Для выхода нажмите Esc. Для продолжения нажмите любую кнопку.");
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+    }
+}
