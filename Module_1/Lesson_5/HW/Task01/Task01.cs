5a624bb6bc216273142f39d92b1369fbba43bcd5
Pastacom <mailkiborg@mail.ru>
2021-09-23 23:08:51 +0300

    .

---
+++ Module_1/Lesson_5/HW/Task01/Task01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static bool Shift(ref char ch)
+    {
+        if ((int)'a' <= (int)ch && (int)ch <= (int)'z')
+        {
+            int code = (int)ch;
+            for (int i = 0; i < 4; i ++)
+            {
+                code += 1;
+            }
+            code = code % (int)'z' < 5? code % (int)'z' + (int)'a' - 1 : code;
+            ch = (char)code;
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+        
+        
+    }
+
+    static void Main()
+    {
+        char ch;
+        do
+        {
+            do
+            {
+                Console.Write("Введите символ: ");
+            } while (!(char.TryParse(Console.ReadLine(), out ch)));
+            bool res = Shift(ref ch);
+            if (res == true)
+                Console.WriteLine($"Новый символ: {ch}");
+            else
+                Console.WriteLine("Неверный ввод");
+            Console.WriteLine("Для выхода нажмите Esc. Для продолжения нажмите любую кнопку.");
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+    }
+}
---
+++ Module_1/Lesson_5/HW/Task03/Task03.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void GcdLcm(uint a, uint b, out uint gcd, out uint lcm)
+    {
+        uint c = 0; gcd = 0; lcm = 0; uint x = a; uint y = b;
+        while (b != 0)
+        {
+            c = a;
+            a = b;
+            b = c % a;
+        }
+        gcd = a > b ? a : b;
+        lcm = x * y / gcd;
+    }
+    static void Main()
+    {
+        uint a; uint b;
+        do
+        {
+            do
+            {
+                Console.Write("Введите значение первого числа: ");
+            } while (!(uint.TryParse(Console.ReadLine(), out a)));
+            do
+            {
+                Console.Write("Введите значение второго числа: ");
+            } while (!(uint.TryParse(Console.ReadLine(), out b)));
+            GcdLcm(a, b, out uint gcd, out uint lcm);
+            Console.WriteLine($"НОД({a}, {b}) = {gcd}\nНОК({a}, {b}) = {lcm}");
+            Console.WriteLine("Для выхода нажмите Esc. Для продолжения нажмите любую кнопку.");
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+    }
+}
