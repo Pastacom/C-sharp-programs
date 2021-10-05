f242cec410276fdd528549e9cf230cdeea50a9f2
Pastacom <mailkiborg@mail.ru>
2021-10-05 19:52:28 +0300

    .

---
+++ Module_1/Lesson_9/CW/Task01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        char[] letters = { 'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+        string input = Console.ReadLine();
+        string[] strings = input.Split(';');
+        for (int i = 0; i < strings.Length; i++)
+        {
+            string[] words = strings[i].Split(' ');
+            StringBuilder result = new StringBuilder();
+            for (int j = 0; j < words.Length; j ++)
+            {
+                words[j] = words[j].ToUpper();
+                for (int z = 0; z < words[j].Length; z ++)
+                {             
+                    if (string.Join("", letters).Contains(words[j][z]))
+                    {
+                        result.Append(words[j][z]);
+                        words[j] = words[j].ToUpper();
+                        break;
+                    }
+                    else
+                    {
+                        result.Append(words[j][z]);
+                        words[j] = words[j].ToLower();
+                    }
+                }
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ Module_1/Lesson_9/CW/Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    static string ConvertHex2Bin(string HexNumber)
+    {
+        string hextable = "123456789ABCDEF";
+        int number = 0;
+        for (int i = 0; i < HexNumber.Length; i ++)
+        {
+            number += (int)Math.Pow(16, i) * (hextable.IndexOf(HexNumber[^(i + 1)]) + 1);
+            Console.WriteLine("" + number);
+        } 
+        StringBuilder binNumber = new StringBuilder();
+        while (number > 0)
+        {
+            binNumber.Append(number % 2);
+            number /= 2;
+        }
+        char[] result = binNumber.ToString().ToCharArray();
+        Array.Reverse(result);
+        return string.Join("", result);
+    }
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(ConvertHex2Bin(input));
+    }
+}
