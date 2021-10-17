68d457ae5e180847e3db7e68510abdfcb4a2776f
Pastacom <mailkiborg@mail.ru>
2021-10-17 15:48:22 +0300

    .

---
+++ Module_1/Lesson_12/CW/Task01/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static bool Palindrome(int number)
+    {
+        string st = number.ToString();
+        if (st[0..(st.Length / 2 + st.Length % 2)] == string.Join("", st[(st.Length / 2)..].Reverse().ToArray()))
+        {
+            return true;
+        }
+        return false;
+    }
+    static int Sum(int number)
+    {
+        int result = 0;
+        while (number > 0)
+        {
+            result += number % 10;
+            number /= 10;
+        }
+        return result;
+    }
+    static int MaxNumber(int number)
+    {
+        int result = 0;
+        while (number > 0)
+        {
+            result = Math.Max(result, number % 10);
+            number /= 10;
+        }
+        return result;
+    }
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        Random rng = new Random();
+        for(int i = 0; i < n; i ++)
+        {
+            arr[i] = rng.Next(1, 10001);
+            Console.Write(arr[i] + " ");
+        }
+        var result = from elem in arr
+                     where (elem % 3 == 0) && (elem.ToString().Length == 2)
+                     select elem;
+        Console.WriteLine();
+        foreach (int elem in result)
+            Console.Write(elem + " ");
+        result = from elem in arr
+                 where Palindrome(elem)
+                 orderby elem
+                 select elem;
+        Console.WriteLine();
+        foreach (int elem in result)
+            Console.Write(elem + " ");
+        result = arr.OrderBy(elem => Sum(elem)).ThenBy(elem => elem);
+        Console.WriteLine();
+        foreach (int elem in result)
+            Console.Write(elem + " ");
+        result = from elem in arr
+                 where elem.ToString().Length == 4
+                 select elem;
+        Console.WriteLine();
+        Console.WriteLine(result.Count() > 0 ? result.Sum() : "Нету чисел длиной 4");
+        result = from elem in arr
+                 where elem.ToString().Length == 2
+                 select elem;
+        Console.WriteLine(result.Count() > 0? result.Min(): "Нету чисел длиной 2");
+        result = from elem in arr
+                 select MaxNumber(elem);
+        foreach (int elem in result)
+            Console.Write(elem + " ");
+    }
+}
---
+++ Module_1/Lesson_12/CW/Task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()
+    {
+        string str = " " + Console.ReadLine() + " ";
+        Regex regex = new Regex(@"\s[a-zA-Zа-яА-Я]{4}\s");
+        var matches = regex.Matches(str);
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+        str = Console.ReadLine();
+        regex = new Regex(@"\w*[aа]{1}");
+        matches = regex.Matches(str);
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match);
+        }
+        str = Console.ReadLine();
+        regex = new Regex(@"\d+");
+        matches = regex.Matches(str);
+        int maxlength = 0;
+        foreach (Match match in matches)
+        {
+            maxlength = Math.Max(maxlength, match.Length);
+        }
+        Console.WriteLine(maxlength);
+    }
+}
