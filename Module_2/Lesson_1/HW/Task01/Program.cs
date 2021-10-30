566b779b1bf63f23a3353358587b1893df84ab2c
Pastacom <mailkiborg@mail.ru>
2021-10-30 20:35:41 +0300

    .

--- Module_2/Lesson_1/HW/Task01/Program.cs
+++ Module_2/Lesson_1/HW/Task01/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 
-namespace Task01
+class Point
 {
-    class Program
+    public double X { get; set; }
+    public double Y { get; set; }
+    public Point(double x = 0, double y = 0)
     {
-        static void Main(string[] args)
+        X = x; Y = y;
+    }
+    public double R
+    {
+        get
         {
-            Console.WriteLine("Hello World!");
+            return Math.Pow(X, 2) + Math.Pow(Y, 2);
         }
+    }
+    public double Fi
+    {
+        get
+        {
+            if (X > 0)
+            {
+                if (Y >= 0)
+                {
+                    return Math.Atan(Y / X);
+                }
+                if (Y < 0)
+                {
+                    return Math.Atan(Y / X) + Math.PI * 2;
+                }
+            }
+            if (X < 0)
+            {
+                return Math.Atan(Y / X) + Math.PI;
+            }
+            else
+            {
+                if (Y > 0)
+                {
+                    return Math.PI / 2;
+                }
+                else if (Y < 0)
+                {
+                    return 3 * Math.PI / 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+    public string PointData
+    {
+        get
+        {
+            return $"X = {X}, Y = {Y}, R = {R}, Fi = {Fi}";
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Point a, b, c, d;
+        a = new Point(3, 2);
+        b = new Point(1, 0);
+        c = new Point();
+        d = new Point();
+        Console.WriteLine(a.PointData);
+        Console.WriteLine(b.PointData);
+        Console.WriteLine(c.PointData);
+        double x, y;
+        do
+        {
+            Console.Write("x = ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            y = double.Parse(Console.ReadLine());
+            d.X = x; d.Y = y;
+            Console.WriteLine(d.PointData);
+        } while (!(x == 0 && y == 0));
     }
 }
---
+++ Module_2/Lesson_1/HW/Task02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+class Polygon
+{
+    public int Edges { get; set; }
+    public double Radius { get; set; }
+    public Polygon(int edges = 0, double radius = 0)
+    {
+        Edges = edges; Radius = radius;
+    }
+    public double Perimeter
+    {
+        get
+        {
+            return Math.Round(Edges * 2 * Radius * Math.Tan(Math.PI / Edges), 5);
+        }
+    }
+    public double Square
+    {
+        get
+        {
+            return Math.Round(Radius * Perimeter / 2, 5);
+        }
+    }
+    public (string, string) PolygonData()
+    {
+        return ($"Количество сторон:{Edges}, Радиус вписанной окружности = {Radius}, Периметр = {Perimeter}, Площадь = ", $"{Square}");
+    }
+}
+// МОДЕРНИЗИРОВАННАЯ ВЕРСИЯ СО СВЯЗНЫМ СПИСКОМ
+class Program
+{
+    static void Main()
+    {
+        int edges;
+        double radius, max = -1, min = -1;
+        LinkedList<Polygon> polygons = new LinkedList<Polygon>();
+        do
+        {
+            Console.Write("Edges = ");
+            edges = int.Parse(Console.ReadLine());
+            Console.Write("Radius = ");
+            radius = double.Parse(Console.ReadLine());
+            if(!(edges == 0 && radius == 0))
+            {
+                Polygon poly = new Polygon(edges, radius);
+                min = min != -1 ? Math.Min(min, poly.Square) : poly.Square;
+                max = max != -1 ? Math.Max(max, poly.Square) : poly.Square;
+                polygons.AddLast(poly);
+            }
+        } while (!(edges == 0 && radius == 0));
+        for (LinkedListNode<Polygon> poly = polygons.First; poly != null; poly = poly.Next)
+        {
+            (string, string) result = poly.ValueRef.PolygonData();
+            Console.Write(result.Item1);
+            if(poly.ValueRef.Square == min)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            if (poly.ValueRef.Square == max)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine(result.Item2);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
